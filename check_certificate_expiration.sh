40056394672cc8246e188b674675cde38ac5994b
nik0chan <nik0chan@hotmail.com>
2021-06-29 12:16:08 +0200

    Changes on chek_certificate_expirtaion script Added timeout, added some checks

--- check_certificate_expiration.sh
+++ check_certificate_expiration.sh
@@ -27,10 +27,11 @@ magenda=`tput setaf 5`
 cyan=`tput setaf 6`
 reset=`tput sgr0`
 AWKCONFIG="./html.awk"
+TIMEPAUSE=0
 
 # PARAMETERS LOAD
 
-while getopts :vsu:f:m:r: option; do
+while getopts :vsu:f:m:r:t: option; do
   case $option in
     v) VERBOSE=1
       [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG] ${green} VERBOSE mode ON ${reset}"
@@ -47,10 +48,13 @@ while getopts :vsu:f:m:r: option; do
     r) REPORT=$OPTARG
       [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG] ${green} REPORTS specified $REPORT ${reset}"
       ;;
-    s) DAEMON=1
+    t) TIMEPAUSE=$OPTARG
+      [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG] ${green} PAUSE BETWEEN CHECKS SET TO $TIMEPAUSE SECONDS ${reset}"
+      ;;
+    s)  DAEMON=1
        REPORT=/tmp/report.html
       [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG] ${green} DAEMON mode ON ${reset}"
-      ;;      
+      ;;
   esac
 done
 
@@ -78,20 +82,20 @@ check_fqdn(){
   echo "${cyan}Checking domain ${green} $1"
   getent hosts $1 > /dev/null
   case $? in
-    0)  EXPDATE=$(echo | openssl s_client -servername $1 -connect $1:443 2>/dev/null | openssl x509 -noout -enddate)
-  	    EXPDATE=$(echo $EXPDATE | cut -d'=' -f2)
-  	    ENDDATE=$(date -d "$EXPDATE" +%s)
-  	    TODAY=$(date +%s)
+    0)  EXPDATE=$(timeout 5 openssl s_client -servername $1 -connect $1:443 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | cut -d'=' -f2)
+        [[ -z "$EXPDATE" ]] && echo "${red}[ERROR]${yellow} Timeout reached, Unable to retrieve certificate information on 5 seconds" && return -1
+  	ENDDATE=$(date -d "$EXPDATE" +%s)
+  	TODAY=$(date +%s)
 
-  	    if [ "$TODAY" -ge "$ENDDATE" ]; then
+        if [ "$TODAY" -ge "$ENDDATE" ]; then
    	      echo "${yellow}EXPIRED!!!!";
    	      echo "${reset}CERTIFICATE IS EXPIRED! ${red} Expiration date: $EXPDATE"
    	      [[ $REPORT ]] && echo "$1 -> <font color="red">EXPIRED!!!</font> $EXPDATE" >> $OUTPUT
-  	    else
+  	else
    	      echo "${reset} Expiration date: $EXPDATE"
    	      [[ $REPORT ]] && echo "$1 -> <font color="black"> $EXPDATE </font>" >> $OUTPUT
-  	    fi
-	      ;;
+  	fi
+	;;
     2)  echo "${yellow}UNKNOWN DOMAIN $1!!!!"
 	      [[ $REPORT ]] && echo "$1 -> <font color="red">UNKOWN DOMAIN!!!</font>" >> $OUTPUT
         ;;
@@ -106,9 +110,10 @@ check_fqdn(){
 # // Main
 
 if [ -z $URL ] && [ -z $FILE ]; then
+  echo "${red}[ERROR]${yellow} you're not in the right path, have you read the manual ? Either you tell me an URL or you give me a file path, you want me to make it up ?" 
   echo "${red}[ERROR]${yellow} No vamos bien, tu te has mirado el manual ? o me dices una URL o me dices un fichero con URLs pero quieres que me las invente?"
   echo "${green}Sintaxis:"
-  echo "${reset}check_certificate_expiration [-v] ${yellow} [-u URL | -f FILE_WITH_URLS]  ${reset} [-s EMAIL_TO] [ -r REPORT_FILE ]"
+  echo "${reset}check_certificate_expiration [-v] ${yellow} [-u URL | -f FILE_WITH_URLS]  ${reset} [-s EMAIL_TO] [ -r REPORT_FILE ] [ -t SECONDS_PAUSE_BETWEEN_DOMAINS]"
   exit 1
 fi
 
@@ -122,13 +127,14 @@ if [ -r "$FILE" ]; then
   [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG]${yellow} Checking URLs on file $FILE ${reset}"
   for i in `cat $FILE` ; do
     check_fqdn $i
+    sleep $TIMEPAUSE
   done
   [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG]${yellow} All domains checked ${reset}"
   else
     check_fqdn $URL 
 fi
 
-if [ ! -z $REPORT ]; then   # HTML Report formatting 
+if [ ! -z $REPORT ]; then   # HTML Report formatting
    [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG]${yellow} formatting $REPORT file ${reset}"
    awk -f $AWKCONFIG $OUTPUT >> $REPORT
    echo "</html>" >> $REPORT
@@ -136,7 +142,7 @@ if [ ! -z $REPORT ]; then   # HTML Report formatting
 fi
 
 if [ $DAEMON ]; then 
-  [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG] ${green} Starting daemon ${reset}"	
+  [[ ! -z $VERBOSE ]] && echo "${cyan}[DEBUG] ${green} Starting daemon ${reset}"
   echo "HTTP/1.1 200 OK
 
         " > /tmp/tempfile
